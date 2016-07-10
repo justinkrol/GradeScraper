26ec40ff4c0ec47b0783aac756033137729eb075
Justin Krol <justinkrol11@gmail.com>
2016-07-10 19:30:10 -0400

    Save to json
    Saves to json file with name of choice

--- scrape.rb
+++ scrape.rb
@@ -1,15 +1,14 @@
 require 'nokogiri'
 require 'http'
 require 'timeout'
+require 'json'
 require 'highline/import'
 require 'pry-byebug'
 
 # CONSTANTS #
-CULEARN_HYPHEN = '–'.freeze # this is a different hyphen than a regular one
-                            # had to copy and paste from CULearn
+CULEARN_HYPHEN = '–'.freeze # this is a different hyphen than a regular one, had to copy and paste from CULearn
 ITEM_MAX_WIDTH = 40 # width of output column for grade item name
-LOGIN_TIMEOUT = 5 # if server is up, should take less than 2 seconds
-
+LOGIN_TIMEOUT = 20 # if server is up, should take less than 2 seconds. CULearn can be really slow sometimes though.
 
 # GLOBAL #
 $cookies = {}
@@ -42,7 +41,7 @@ def login
         )
       end
     rescue Timeout::Error
-      puts 'Could not reach the CULearn server. It is probably down.'
+      puts 'Could not reach the CULearn server. The server may be down.  Check your internet connection.'
       return :abort
     end
     response = perform_login_redirect(login_response.to_s)
@@ -80,7 +79,6 @@ end
 
 ####### Program Start ##########
 
-
 # num_semesters = ask('# Semesters: ')
 
 puts 'CULearn Grade Scraper'
@@ -101,35 +99,69 @@ courses_page.css('.courses .course').each do |course|
 end
 puts 'Displaying grades'
 puts ''
+
+results = {} # empty hash, will fill in loop below
+# format:
+# results: {
+#   course_id: {
+#     name: "",
+#     code: "",
+#     items: {
+#       item_id: {
+#         name: "",
+#         grade: "",
+#         max: ""
+#       }
+#     }
+#   }
+# }
+current_course_id = 0
 courses.each do |course_id|
+  current_item_id = 0
   grade_page = get_grade_report(course_id)
   if grade_page.xpath('//text()').to_s.include? 'Grader report'
     puts 'TA course: skipped'
     puts ''
     next
   end
+  course_name = '' # visible throughout loop
   grade_page.css('.generaltable.user-grade tbody tr').each_with_index do |grade_item, i|
     next if grade_item.css('th.column-itemname').to_s.strip == '' # if CULearn has an empty tr for some reason
     if i.zero?
-      puts "\nCourse: " + grade_item.css('th.column-itemname').text
+      course_name = grade_item.css('th.column-itemname').text
+      results[current_course_id] = { name: course_name, items: {} }
+      puts "\nCourse: " + course_name
       printf "%-#{ITEM_MAX_WIDTH}s %s\n", 'Name', 'Grade'
     else
       name = grade_item.css('th.column-itemname').text
-      printf "%-#{ITEM_MAX_WIDTH}s", name[0..ITEM_MAX_WIDTH-2]
-      print grade_item.css('td.column-grade').text
+      printf "%-#{ITEM_MAX_WIDTH}s", name[0..(ITEM_MAX_WIDTH - 2)]
+      grade = grade_item.css('td.column-grade').text
+      print grade
       range = grade_item.css('td.column-range').text
       if range.strip != ''
         if range.split(CULEARN_HYPHEN).empty?
           print 'NA'
         else
-          # print "|#{range}|"
-          # print "~~#{range.split(CULEARN_HYPHEN)}~~"
-          print '/' + range.split(CULEARN_HYPHEN).last
+          max = range.split(CULEARN_HYPHEN).last
+          print '/' + max
         end
       end
+      results[current_course_id][:items][current_item_id] = { name: name, grade: grade, max: max }
       puts ''
     end
+    current_item_id += 1
   end
+  current_course_id += 1
   puts ''
 end
+
+save_json = ask 'Output to json? (y/n)'
+if save_json.downcase.include? 'y'
+  json_filename = ask('Name of json file?').chomp '.json'
+  puts "Saving to #{json_filename}.json"
+  File.open("#{json_filename}.json", 'w') do |file|
+    file.write(JSON.pretty_generate(results))
+  end
+end
+
 puts 'Finished'
